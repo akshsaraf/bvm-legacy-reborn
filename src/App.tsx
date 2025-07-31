7adf62e53149536d3a2871ec15f0e7e15de222b8
Aksh Saraf <akshsaraf007@gmail.com>
2025-07-31 11:31:13 +0000

    something

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-
+import ScrollToTop from "@/components/ScrollToTop"; // adjust path if needed
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -22,6 +22,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+      <ScrollToTop/>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
